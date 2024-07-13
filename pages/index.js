977b10c90c05ac6ea0ef8bc9d8e378ee2bc651b3
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-07-14 00:41:21 +0530

    Add : background image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,74 @@
+import Image from "next/image";
+
+// components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+// framer motion
+import { motion } from "framer-motion";
+
+// variants
+import { fadeIn } from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* title */}
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Hi, I am<br /><span className="text-accent">Shubham Patel</span>
+          </motion.h1>
+          {/* subtitle */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            I am a developer who is very curious to know about new technologies,
+            I am very innovative and motivated about creating, scaling, and
+            optimizing solutions.
+          </motion.p>
+          {/* Button */}
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      {/* image */}
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg img */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        <div>
+          {/* particles */}
+          <div>Particles</div>
+          {/* avtar */}
+          <div className="w-full h-full max-w-[637px] max-h-[878px] absolute -bottom-32 lg:bottom-0 lg:right-[17%] ">
+          <Avatar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
