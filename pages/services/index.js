c22dfa0834370a29bcf723be4bf371a2fc7f6a59
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-07-19 12:27:14 +0530

    Add: Service Slider working properly

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,42 +1,3 @@
-// icons
-import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-} from "react-icons/rx";
-
-
-// data
-const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-];
-
 //Components
 import ServiceSlider from "../../components/ServiceSlider";
 import Bulb from '../../components/Bulb';
@@ -53,16 +14,28 @@ const Services = () => {
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-x-8'>
           <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-            <h2 className='h2 xl:mt-8'>
+            <motion.h2
+              variants={fadeIn('up',0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className='h2 xl:mt-8'>
               My services <span className='text-accent'>.</span>
-            </h2>
-            <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn('up',0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. A
               eveniet dignissimos distinctio tempore harum laudantium.
-            </p>
+            </motion.p>
           </div>
-          {/* slider */}
+          <div className="w-full xl:max-w-[65%]">
+            {/* slider */}
           <ServiceSlider />
+          </div>
         </div>
       </div>
       <Bulb />
