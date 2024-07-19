c9eca0b1016be22065833001cdd92e2e8c725fd1
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-07-19 11:30:42 +0530

    Add : heading to complete the service slider

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,41 +1,19 @@
-// icons
+//swiper react components
+import {Swiper, SwiperSlide} from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+//icons
 import {
   RxCrop,
-  RxPencil2,
   RxDesktop,
+  RxPencil2,
   RxReader,
   RxRocket,
-} from "react-icons/rx";
-
-
-// data
-const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-];
+  RxArrowTopLeft,
+} from 'react-icons/rx';
 
 const ServiceSlider = () => {
   return <div>Service Slider</div>;
